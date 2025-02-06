7e2c3d3e7891fdabb7b562693404e28524b053ed
Takib-Ahmed <satakib300@gmail.com>
2025-02-06 06:12:59 +0600

    react icons and skills update

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,60 +2,44 @@
 import { useEffect, useState } from "react";
 import { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faReact } from "@fortawesome/free-brands-svg-icons";
 import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
-import { faCss3 } from "@fortawesome/free-brands-svg-icons";
-import { faJs } from "@fortawesome/free-brands-svg-icons";
-import { faBootstrap } from "@fortawesome/free-brands-svg-icons";
-import { faGit } from "@fortawesome/free-brands-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-
-
-
+import { SiNextui } from "react-icons/si";
+import { SiNextdotjs } from "react-icons/si";
+import { SiTailwindcss } from "react-icons/si";
+import { BiLogoTypescript } from "react-icons/bi";
+import { SiExpress } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiShadcnui } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiCss3 } from "react-icons/si";
+import { FaBootstrap } from "react-icons/fa";
+import { FaGit } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 const Skills = () => {
   const [listskills] = useState([
-    { name: "HTML", icon: faHtml5 },
-    {
-      name: "CSS",
+    { name: "Next.js", icon: SiNextdotjs, color: '#fff', size: 85 },
+    { name: "React.js", icon: FaReact, color: '#61DAFB', size: 85 },
+    { name: "TypeScript", icon: BiLogoTypescript, color: '#3178C6', size: 90},
+    { name: "JavaScript", icon: IoLogoJavascript, color: '#F7DF1E', size: 85 },
+   
+    { name: "Shadcn", icon: SiShadcnui, color: '#fff', size: 80 },
+    { name: "Next UI", icon: SiNextui, color: '#fff', size: 85 },
+    { name: "Tailwind CSS", icon: SiTailwindcss, color: '#38BDF8', size: 85 },
+    { name: "Bootstrap", icon: FaBootstrap, color: '#7711ED', size: 85 },
+    { name: "Node.js", icon: FaNodeJs, color: 'green', size: 85 },
+    { name: "Express", icon: SiExpress, color: '#fff', size: 85 },
+    { name: "MongoDB", icon: SiMongodb, color: 'green', size: 85 },
 
-      icon: faCss3,
-    },
-
-    {
-      name: "Bootstrap",
-
-      icon: faBootstrap,
-    },
-    {
-      name: "talwindcss",
-      icon: "",
-    },
-    {
-      name: "Javascript",
-
-      icon: faJs,
-    },
-    {
-      name: "Reactjs",
-
-      icon: faReact,
-    },
-    {
-      name: "git",
-
-      icon: faGit,
-    },
-    {
-      name: "Github",
-
-      icon: faGithub,
-    },
-    {
-      name: "VS CODE",
-
-      icon: "",
-    },
+    { name: "Git", icon: FaGit, color: '#F05032', size: 85 },
+    { name: "GitHub", icon: FaGithub, color: '#fff', size: 85 },
+    { name: "CSS", icon: SiCss3, color: '#1572B6', size: 85 },
+    { name: "HTML", icon: faHtml5, color: '#E34F26', size: 85 },
+    { name: "VS Code", color: '#007ACC', size: 85 },
   ]);
+  
+  
   // Create a ref to store the list items
   const itemRefs = useRef([]);
 
@@ -99,7 +83,7 @@ const Skills = () => {
   }, []);
   return (
     <div className=" ">
-      <section className="skills my-10 mt-20" id="skills">
+      <section className="skills my-10 " id="skills">
         <div className="des">My Skills</div>
         <div className="title">Technologies That I know</div>
         <br />
@@ -116,7 +100,21 @@ const Skills = () => {
                 ref={(el) => (itemRefs.current[key] = el)} // Assign refs to each item
                 className="item Hidden" // Default class is Hidden
               >
-                <FontAwesomeIcon icon={value.icon} ></FontAwesomeIcon>
+{value.icon ? 
+  (value.name === 'HTML' ? 
+    <FontAwesomeIcon icon={value.icon}    style={{ color: value.color , fontSize:'90px'}} /> 
+    : <value.icon 
+     
+        className={`shadow  `} 
+        style={{ color: value.color }} 
+        size={value.size} 
+        
+      /> 
+  ) 
+  : <div className="h-8" />
+}
+
+         
                 <h3>{value.name}</h3>
                 <div className="des">{value.des}</div>
               </div>
