7189a02473fde43a838be14fa975088c4f9c11fd
Takib-Ahmed <satakib300@gmail.com>
2025-03-14 07:23:27 +0600

    skill progress height view adjusted

--- src/components/Reactprogress.jsx
+++ src/components/Reactprogress.jsx
@@ -32,7 +32,7 @@ export default function  RectProgresss({screenSize,value,Revealed,KEY}){
        <svg 
          fill="transparent"
          id="svg" 
-         viewBox={screenSize>520 ?"0 0 110 117" :"0 0 95 110"}
+         viewBox={screenSize>520 ?"0 0 105 117" :"0 0 85 110"}
         
          xmlns="http://www.w3.org/2000/svg" 
          className="w-full h-full   rounded-[10%]    top-0 absolute left-0 SVG    "
