02f1887a78f3fb49e0eeaf78cbeb3b860772e112
Takib-Ahmed <satakib300@gmail.com>
2025-03-23 19:21:22 +0600

    test 2

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,10 +6,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import Download from "../assets/download (1).png"
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
-import { LuMessageCircleX } from "react-icons/lu";
-import { LuMessageCircleMore } from "react-icons/lu";
-import { LuMessageCirclePlus } from "react-icons/lu";
-import { FaWhatsapp } from 'react-icons/fa'
+import { RiMessage3Fill } from "react-icons/ri";
 const Home = () => {
 
   return (
@@ -46,18 +43,13 @@ const Home = () => {
       </section>
    <div className='  hidden fixed   bottom-5 right-5 flex bg-[#010824] invert w-[6rem] z-50 rounded-full  p-2  items-center justify-between'>
 
- connect   <LuMessageCirclePlus />
+ connect   <RiMessage3Fill />
    </div>
-<div className='  fc-container fixed right-0 top-0  h-screen  flex justify-end items-end lg:justify-center lg:items-center  z-50 '>
-   <div className="links fixedcontracts  lg:mt-32   p-4 lg:p-1    flex flex-col  justify-start  lg:justify-center gap-5 sm:gap-10    w-max  items-center   rounded-r-[3.5rem]">
+<div className='  fc-container fixed right-0 top-0  h-full  flex justify-end items-end lg:justify-center lg:items-center  z-50 '>
+   <div className="links fixedcontracts   p-4 lg:p-1   lg:mt-14    flex flex-col  justify-start  gap-5 sm:gap-10    w-max  items-center   rounded-r-[3.5rem]">
         
-        {/* <a href="/CV/Sa Takib.pdf" className='cv hidden hover:text-black  text-[20px] px-2 py-[0.45rem] rounded-full' download  ><div className=' grid  items-center  sm:gap-2'><p>CV</p> <img src={Download} alt="" className=' hidden   invert ' /></div></a> */}
-        <a href="" className=' absolute  -translate-y-full right-4  lg:relative rounded-[100%]  px-2 py-[0.45rem] text-2xl lg:text-3xl flex justify-center items-center  '><FontAwesomeIcon href='#' icon={faLinkedin}/></a><a href='https://github.com/Takib-Ahmed'  className='flex justify-center items-center absolute  -translate-y-full right-4  lg:relative rounded-[100%] text-2xl lg:text-3xl px-2 py-[0.45rem] '><FontAwesomeIcon icon={faGithub}/></a><a href='https://www.instagram.com/lg.takeb'  className=' flex justify-center items-center px-2 py-[0.45rem]  absolute  -translate-y-full right-4  lg:relative rounded-[100%] text-2xl lg:text-3xl '><FontAwesomeIcon icon={faInstagram}/></a><a href='https://wa.me/01615081141' className='flex justify-center items-center px-2 py-[0.45rem] absolute  -translate-y-full right-4  lg:relative rounded-[100%] text-2xl lg:text-3xl  ' ><FontAwesomeIcon  icon={faWhatsapp}/></a>
-        
-        <a  className='flex   justify-center items-center px-2 py-[0.45rem] absolute  -translate-y-full right-4  lg:relative rounded-[100%] text-2xl lg:text-3xl  ' > <FaWhatsapp /></a>
-        <a  className='flex   justify-center items-center px-2 py-[0.45rem] absolute  -translate-y-full right-4  lg:relative rounded-[100%] text-2xl lg:text-3xl  ' > <LuMessageCircleX /></a>
-        
-        </div></div>
+        <a href="/CV/Sa Takib.pdf" className='cv hidden hover:text-black  text-[20px] px-2 py-[0.45rem] rounded-full' download  ><div className=' grid  items-center  sm:gap-2'><p>CV</p> <img src={Download} alt="" className=' hidden   invert ' /></div></a>
+        <a href="" className=' absolute  -translate-y-full right-4  lg:relative rounded-[100%]  px-2 py-[0.45rem] text-2xl lg:text-3xl flex justify-center items-center  '><FontAwesomeIcon href='#' icon={faLinkedin}/></a><a href='https://github.com/Takib-Ahmed'  className='flex justify-center items-center absolute  -translate-y-full right-4  lg:relative rounded-[100%] text-2xl lg:text-3xl px-2 py-[0.45rem] '><FontAwesomeIcon icon={faGithub}/></a><a href='https://www.instagram.com/lg.takeb'  className=' flex justify-center items-center px-2 py-[0.45rem]  absolute  -translate-y-full right-4  lg:relative rounded-[100%] text-2xl lg:text-3xl '><FontAwesomeIcon icon={faInstagram}/></a><a href='https://wa.me/01615081141' className='flex justify-center items-center px-2 py-[0.45rem] absolute  -translate-y-full right-4  lg:relative rounded-[100%] text-2xl lg:text-3xl  ' ><FontAwesomeIcon  icon={faWhatsapp}/></a></div></div>
     </div>)
 
 }
